78f2bc79a66839e0803e7d322928b7b92fafb4fe
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-11 09:33:49 -0400

    3 passed 6 failed

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
